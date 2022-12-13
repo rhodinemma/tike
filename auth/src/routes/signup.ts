007647b93c49348133133574054b612783c27f35
rhodinemma <rhodinemma10@gmail.com>
2022-12-13 17:39:07 +0300

    ch(handling-validation-errors)

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -7,7 +7,17 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
 ], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array())
+    }
+
     const { email, password } = req.body;
+
+    console.log('Creating a user...');
+
+    res.send({});
 })
 
 export { router as signupRouter };
