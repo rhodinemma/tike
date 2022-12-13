af2610ed2246c6786f50cdee0287cf17ea444ca0
rhodinemma <rhodinemma10@gmail.com>
2022-12-13 17:17:39 +0300

    ft(scaffolding-routes)

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', () => {
-
+router.get('/api/users/currentuser', (req, res) => {
+    res.send('Hello there');
 })
 
 export { router as currentUserRouter };
