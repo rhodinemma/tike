d39201e97e08386db646093b995f87a072e08d74
rhodinemma <rhodinemma10@gmail.com>
2022-12-14 16:44:00 +0300

    ft(extra-doc-props)

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,6 +7,19 @@ interface UserAttributes {
     password: string;
 }
 
+// an interface that describes properties
+// a user model has
+interface UserModel extends mongoose.Model<UserDocument> {
+    build(attributes: UserAttributes): UserDocument;
+}
+
+// an interface that describes the properties
+// a User document has
+interface UserDocument extends mongoose.Document {
+    email: string,
+    password: string,
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -18,10 +31,10 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attributes: UserAttributes) => {
+userSchema.statics.build = (attributes: UserAttributes) => {
     return new User(attributes);
-};
+}
 
-export { User, buildUser };
+const User = mongoose.model<UserDocument, UserModel>('User', userSchema);
+
+export { User };
