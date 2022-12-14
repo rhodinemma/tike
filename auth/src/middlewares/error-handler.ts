75357d86894d427c7079433ea576cc73745520e7
rhodinemma <rhodinemma10@gmail.com>
2022-12-14 11:16:46 +0300

    ft(subclass-custom-errors)

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,5 +3,5 @@ import { Request, Response, NextFunction } from 'express';
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     console.log('Something went wrong', err);
 
-    res.status(400).send({ message: 'Something went wrong' })
+    res.status(400).send({ message: err.message })
 };
