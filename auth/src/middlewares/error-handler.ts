56d5be70f40d933688cc765f5fdfb00c12fc4f3e
rhodinemma <rhodinemma10@gmail.com>
2022-12-14 11:56:03 +0300

    ft(convert-errors-to-responses)

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -4,11 +4,23 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof RequestValidationError) {
-        console.log('Handling this error as a request valdiation error');
+        const formattedErrors = err.errors.map(error => {
+            return {
+                message: error.msg,
+                field: error.param
+            };
+        });
+        return res.status(400).send({ errors: formattedErrors });
     }
 
     if (err instanceof DatabaseConnectionError) {
-        console.log('Handling this error as a db connection error');
+        return res.status(500).send({
+            errors: [
+                {
+                    message: err.reason
+                }
+            ]
+        })
     }
 
     res.status(400).send({ message: err.message })
