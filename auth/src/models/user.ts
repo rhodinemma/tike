b2b2d7b0798d23def4deec485809cf5d0209e3d2
rhodinemma <rhodinemma10@gmail.com>
2022-12-14 17:57:19 +0300

    ft(mongoose-pre-save-hooks)

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../helpers/password';
 
 // an interface that describes properties
 // required to create a new user
@@ -29,6 +30,14 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre('save', async function (done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
 });
 
 userSchema.statics.build = (attributes: UserAttributes) => {
