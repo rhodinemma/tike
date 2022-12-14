3ec83c17827f1ce1e63c491bb413a6aa8397b302
rhodinemma <rhodinemma10@gmail.com>
2022-12-14 11:31:21 +0300

    ft(determine-error-types)

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('Something went wrong', err);
+    if (err instanceof RequestValidationError) {
+        console.log('Handling this error as a request valdiation error');
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log('Handling this error as a db connection error');
+    }
 
     res.status(400).send({ message: err.message })
 };
