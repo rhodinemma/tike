f6dc3eba1afb081b5d68135658f494d388f5cdc3
rhodinemma <rhodinemma10@gmail.com>
2022-12-16 10:37:13 +0300

    ft(access-env-in-pod)

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -33,7 +33,7 @@ router.post('/api/users/signup', [
     const userJwt = jwt.sign({
         id: user.id,
         email: user.email
-    }, 'asdf')
+    }, process.env.JWT_KEY!)
 
     // store it on session object
     req.session = {
