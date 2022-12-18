aa0f43d677a372a45c9889b45a8335aa0169faa5
rhodinemma <rhodinemma10@gmail.com>
2022-12-18 11:01:23 +0300

    ch(ignore-dev-deps)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { app } from './app';
 
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-)
 mongoose.set('strictQuery', true);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-})
-
-app.use(errorHandler);
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
