d9a5d7efc5e3618b145aa2c37d38394406a3d79f
rhodinemma <rhodinemma10@gmail.com>
2022-12-18 16:28:26 +0300

    ch(test-invalid-input)

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -3,4 +3,17 @@ import { app } from '../../app';
 
 it('returns 201 on successful signup', async () => {
     return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app).post('/api/users/signup').send({ email: 'testtest.com', password: 'password' }).expect(400);
+})
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'p' }).expect(400);
+})
+
+it('returns a 400 with missing email and password', async () => {
+    await request(app).post('/api/users/signup').send({ email: '' }).expect(400);
+    await request(app).post('/api/users/signup').send({ password: '' }).expect(400);
 })
