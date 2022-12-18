ba7381552e5b00bd40ec153c41987d363c6959c2
rhodinemma <rhodinemma10@gmail.com>
2022-12-18 11:58:46 +0300

    ch(setup-test-env)

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,6 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns 201 on successful signup', async () => {
+    return request(app).post('/api/users/signup').send({ email: 'test@test.com', password: 'password' }).expect(201);
+})
