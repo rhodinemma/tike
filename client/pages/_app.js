8535ba2619496ebe09b3f5cc639c6e1dff1e8a45
rhodinemma <rhodinemma10@gmail.com>
2022-12-31 20:35:39 +0300

    ch(add-styling)

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import { Andika } from "@next/font/google";
+
+const andika = Andika({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+  return (
+    <main className={andika.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
