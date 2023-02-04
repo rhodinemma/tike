727c0f03028250663601ad2bafc1ef0344f3c1c4
rhodinemma <rhodinemma10@gmail.com>
2023-02-04 23:40:47 +0300

    ft(client-ingress-auth-communication-with-cookie)

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,13 +8,19 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Pages</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
+  //console.log(req.headers);
   if (typeof window === "undefined") {
     // we are on the server
     // requests should be made to a namespace
     const { data } = await axios.get(
-      "http://SERVICENAME.NAMESPACE.svc.cluster.local"
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
     );
+
+    return data;
   } else {
     // we are on the browser
     // requests can be made with a base url of ''
