d7d293fa681fcef58f79bd5372de5bd100fbb071
rhodinemma <rhodinemma10@gmail.com>
2023-02-04 23:58:50 +0300

    ft(reusable-api-client)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
@@ -8,26 +8,11 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Pages</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  //console.log(req.headers);
-  if (typeof window === "undefined") {
-    // we are on the server
-    // requests should be made to a namespace
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
 
-    return data;
-  } else {
-    // we are on the browser
-    // requests can be made with a base url of ''
-    const { data } = await axios.get("/api/users/currentuser");
-    // { currentuser: {}}
-    return data;
-  }
+  return data;
 };
 
 export default LandingPage;
