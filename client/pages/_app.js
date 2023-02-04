372c49dc15f20a18af80ff9dbca0348199c37261
rhodinemma <rhodinemma10@gmail.com>
2023-02-05 00:53:17 +0300

    ft(handling-multiple-getInitialProps)

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -19,6 +19,13 @@ AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx);
   const { data } = await client.get("/api/users/currentuser");
 
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
   return data;
 };
 
