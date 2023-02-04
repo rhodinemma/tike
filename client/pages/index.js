418a4f91f1f9736c57850f0a7c6e64d99992bf5f
rhodinemma <rhodinemma10@gmail.com>
2023-02-05 00:14:30 +0300

    ft(some-landing-page-content)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,11 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  // axios.get("/api/users/currentuser").catch((err) => {
-  //   console.log(err.message);
-  // });
-  return <h1>Landing Pages</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
