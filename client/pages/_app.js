ab02056cc2d5bf9f7fffc716dcc63f8e5b78a8b5
rhodinemma <rhodinemma10@gmail.com>
2023-02-05 00:39:22 +0300

    ft(move-getInitialProps)

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,15 +1,25 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { Andika } from "@next/font/google";
+import buildClient from "../api/build-client";
 
 const andika = Andika({
   weight: "400",
   subsets: ["latin"],
 });
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
   return (
     <main className={andika.className}>
       <Component {...pageProps} />
     </main>
   );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default AppComponent;
